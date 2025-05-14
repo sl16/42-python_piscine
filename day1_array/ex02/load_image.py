46fb8174843fda8be2e979e4fc26536e431744f9
sl16 <vojtabar119@gmail.com>
2025-05-14 14:07:44 +0200

    day1 ex02 finished

---
+++ day1_array/ex02/load_image.py
@@ -0,0 +1,31 @@
+import numpy as np
+from PIL import Image
+
+def ft_load(path: str) -> np.ndarray:
+    
+    """
+    Loads an image, prints its format, and its pixels
+    content in RGB format.
+
+    Handles JPG and JPEG.
+    """
+    try:
+        img = Image.open(path)
+        assert img is not None, ("Cannot load image")
+        assert img.format in ("JPG", "JPEG"), ("Provide JPG or JPEG only")
+
+        print(f"The shape of the {img.format} image is: "
+                f"({img.height}, {img.width}, {len(img.mode)})")
+        return (np.array(img))
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ day1_array/ex02/tester.py
@@ -0,0 +1,9 @@
+from load_image import ft_load
+
+print(ft_load("landscape.jpg"))
+print("\n------------\n")
+print(ft_load("animal.jpeg"))
+print("\n------------\n")
+print(ft_load("stem.png"))
+print("\n------------\n")
+print(ft_load("kokos.jpg"))
